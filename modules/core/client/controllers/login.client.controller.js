43fbee98d6f7e2bc5f57617cccce83c85b8bbf68
hamt3ch <hmiles23@ufl.edu>
2015-10-01 19:26:26 -0400

    connected View to controller.variables

---
+++ modules/core/client/controllers/login.client.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('core').controller('LoginController', ['$scope',
+                /*'Authentication',*/
+              	//function ($scope, Authentication) {
+                	// This provides Authentication context.
+                	//scope.authentication = Authentication;
+    function ($scope) {
+      $scope.usernameField = "";
+      $scope.passwordField = "";
+
+      $scope.login = function(usernameField, passwordField) {
+        console.log("Pressed Submit");
+
+      }
+    }
+]);

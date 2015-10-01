a8f452e3f797d6d166859cf770ea9b91a47f26ed
Miles <hmiles23@msl-2-a04.lab.at.ufl.edu>
2015-10-01 19:45:44 -0400

    Setup user router

--- modules/core/client/controllers/login.client.controller.js
+++ modules/core/client/controllers/login.client.controller.js
@@ -1,5 +1,23 @@
 'use strict';
 
+var express = require('express'),
+    router = express.Router(),
+    passport = require('passport');
+    User = require('../models/user.js');
+
+
+router.post('/register', function(req, res) {
+  User.register(new User({ username: req.body.username }), req.body.password, function(err, account) {
+    if (err) {
+      return res.status(500).json({err: err})
+    }
+    passport.authenticate('local')(req, res, function () {
+      return res.status(200).json({status: 'Registration successful!'})
+    });
+  });
+});
+
+
 angular.module('core').controller('LoginController', ['$scope',
                 /*'Authentication',*/
               	//function ($scope, Authentication) {
@@ -11,7 +29,16 @@ angular.module('core').controller('LoginController', ['$scope',
 
       $scope.login = function(usernameField, passwordField) {
         console.log("Pressed Submit");
-
+          router.post('/register', function(req, res) {
+            User.register(new User({ username: req.body.username }), req.body.password, function(err, account) {
+              if (err) {
+                return res.status(500).json({err: err})
+              }
+              passport.authenticate('local')(req, res, function () {
+                return res.status(200).json({status: 'Registration successful!'})
+              });
+            });
+          });
       }
     }
 ]);

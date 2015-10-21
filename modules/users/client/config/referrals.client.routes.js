60635da50cdef01fd56a69bf759c7d69ee6a2375
hamt3ch <hmiles23@ufl.edu>
2015-10-21 15:13:06 -0400

    Created views for Admin to edit,delete,view, and create Referrals Issues: * The views need to copied from articles page to have functionality

--- modules/users/client/config/referrals.client.routes.js
+++ modules/users/client/config/referrals.client.routes.js
@@ -7,30 +7,45 @@ angular.module('referrals').config(['$stateProvider',
     $stateProvider
       .state('referrals', {
         abstract: true,
-        url: '/employees/referrals',
+        url: '/referrals',
         template: '<ui-view/>',
         data:{
           roles:['admin', 'user']
         }
       })
+
+      //safetyNet
+      .state('referrals.safetyNet', {
+        url: '/',
+        templateUrl: 'modules/users/client/views/employees/list-referrals.client.view.html',
+      })
+
+      //Both Users
       .state('referrals.list', {
         url: '',
-        templateUrl: 'modules/users/client/views/employees/list-referrals.client.view.html'
+        templateUrl: 'modules/users/client/views/employees/list-referrals.client.view.html',
+        data: {
+          roles: ['admin', 'user']
+        }
       })
+      //AdminViews
       .state('referrals.create', {
         url: '/create',
-        templateUrl: 'modules/users/client/views/employees/list-referrals.client.view.html',
+        templateUrl: 'modules/users/client/views/admin/create-referral.client.view.html',
         data: {
           roles: ['admin']
         }
       })
       .state('referrals.view', {
         url: '/:referralId',
-        templateUrl: 'modules/users/client/views/employees/list-referrals.client.view.html'
+        templateUrl: 'modules/users/client/views/admin/view-referral.client.view.html',
+        data: {
+          roles: ['admin']
+        }
       })
       .state('referrals.edit', {
         url: '/:referralId/edit',
-        templateUrl: 'modules/users/client/views/employees/list-referrals.client.view.html',
+        templateUrl: 'modules/users/client/views/admin/edit-referral.client.view.html',
         data: {
           roles: ['admin']
         }

82cd62b0e0e4ada160a1244d74fcb119dd9e1b85
Diego Nolasco <dnol777@ufl.edu>
2015-10-27 17:28:53 -0400

    Added search need to work on routes

--- modules/core/client/controllers/career.client.controller.js
+++ modules/core/client/controllers/career.client.controller.js
@@ -1,11 +1,13 @@
 'use strict';
 
-angular.module('core').controller('CareersController', ['$scope', '$rootScope', 'Jobs',
+angular.module('core').controller('CareersController', ['$scope', '$filter', '$rootScope', 'Jobs',
 
-    function ($scope, $rootScope, Jobs) {
+    function ($scope, $filter, $rootScope, Jobs) {
 
         $scope.Jobs = [];
-
+        $scope.itemsPerPage = 15;
+        $scope.currentPage = 1;
+        $scope.pagedItems = [];
         $scope.careerApplicationVisible = false;
 
         $scope.openCareerApplication = function() {
@@ -28,5 +30,18 @@ angular.module('core').controller('CareersController', ['$scope', '$rootScope', 
           $scope.Jobs = Jobs.query();
         };
 
+        $scope.figureOutItemsToDisplay = function () {
+          $scope.filteredItems = $filter('filter')($scope.Jobs, {
+            $: $scope.search
+          });
+          $scope.filterLength = $scope.filteredItems.length;
+          var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+          var end = begin + $scope.itemsPerPage;
+          $scope.pagedItems = $scope.filteredItems.slice(begin, end);
+        };
+
+        $scope.pageChanged = function () {
+          $scope.figureOutItemsToDisplay();
+        };
 
   	}]);

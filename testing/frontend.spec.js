5a5e7026793a9cd4a309d506c0e0709464db0354
hamt3ch <hmiles23@ufl.edu>
2015-12-08 20:54:54 -0500

    Frontend testing works

--- testing/frontend.spec.js
+++ testing/frontend.spec.js
@@ -14,51 +14,46 @@ var addUser = function() {
   element(by.model('credentials.username')).sendKeys('admin');
   element(by.model('credentials.password')).sendKeys('AdminPassword1!');
   element(by.model('submitSignIn')).click();
-}
+};
 
 var addJob = function() {
   element(by.model('signInBtn')).click();
   element(by.model('credentials.username')).sendKeys('admin');
   element(by.model('credentials.password')).sendKeys('AdminPassword1!');
   element(by.model('submitSignIn')).click();
-}
+};
 
 var addReferral = function() {
   element(by.model('signInBtn')).click();
   element(by.model('credentials.username')).sendKeys('admin');
   element(by.model('credentials.password')).sendKeys('AdminPassword1!');
   element(by.model('submitSignIn')).click();
-}
+};
 
 var addContact = function() {
   element(by.model('signInBtn')).click();
   element(by.model('credentials.username')).sendKeys('admin');
   element(by.model('credentials.password')).sendKeys('AdminPassword1!');
   element(by.model('submitSignIn')).click();
-}
-
-describe('SignIn Users', function() {
-  it('Open Signin box', function() {
-    browser.get('http://localhost:3000/');
-
-    loginAsAdmin();
-}}));
+};
 
 describe('Clicking all the buttons', function(){
+  browser.get('http://localhost:3000/');
+    
   it('Navigate to About Us', function() {
-    element(by.model('aboutUs')).click;
-    element(by.model('home')).click;
+    element(by.model('aboutUs')).click();
+    element(by.model('home')).click();
   });
-  it('Navigate to About Us', function() {
-    element(by.model('services')).click;
-    element(by.model('home')).click;
+  it('Navigate to services', function() {
+    element(by.model('services')).click();
+    element(by.model('home')).click();
   });
-  it('Navigate to About Us', function() {
-    element(by.model('packages')).click;
-    element(by.model('home')).click;
+  it('Navigate to packages', function() {
+    element(by.model('packages')).click();
+    element(by.model('home')).click();
   });
-  it('Navigate to About Us', function() {
-    element(by.model('careers')).click;
-    element(by.model('home')).click;
+  it('Navigate to careers', function() {
+    element(by.model('careers')).click();
+    element(by.model('home')).click();
   });
 });

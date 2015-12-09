2c31007fa21c73b607db2ed44d0771868626a6cd
Daniel Lelis <oheyitsdaniel@gmail.com>
2015-12-08 19:46:01 -0500

    Starting new test.js file

---
+++ testing/frontend.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+browser.driver.manage().window().maximize();
+
+var loginAsAdmin = function() {
+  //login in as admin
+  element(by.model('signInBtn')).click();
+  element(by.model('credentials.username')).sendKeys('admin');
+  element(by.model('credentials.password')).sendKeys('AdminPassword1!');
+  element(by.model('submitSignIn')).click();
+};
+
+var addJob = function() {
+  element(by.model('signInBtn')).click();
+  element(by.model('credentials.username')).sendKeys('admin');
+  element(by.model('credentials.password')).sendKeys('AdminPassword1!');
+  element(by.model('submitSignIn')).click();
+}
+
+var addReferral = function() {
+  element(by.model('signInBtn')).click();
+  element(by.model('credentials.username')).sendKeys('admin');
+  element(by.model('credentials.password')).sendKeys('AdminPassword1!');
+  element(by.model('submitSignIn')).click();
+}
+
+var addUser = function() {
+  element(by.model('signInBtn')).click();
+  element(by.model('credentials.username')).sendKeys('admin');
+  element(by.model('credentials.password')).sendKeys('AdminPassword1!');
+  element(by.model('submitSignIn')).click();
+}
+
+describe('SignIn Users', function() {
+  it('Open Signin box', function() {
+    browser.get('http://localhost:3000/');
+
+    loginAsAdmin();

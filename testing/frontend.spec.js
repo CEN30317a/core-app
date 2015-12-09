64c9348bd8b41ef36e1dd76e9664b9ea83d0b335
Daniel Lelis <oheyitsdaniel@gmail.com>
2015-12-08 20:43:01 -0500

    Writing small test file for Hugh

--- testing/frontend.spec.js
+++ testing/frontend.spec.js
@@ -9,6 +9,13 @@ var loginAsAdmin = function() {
   element(by.model('submitSignIn')).click();
 };
 
+var addUser = function() {
+  element(by.model('signInBtn')).click();
+  element(by.model('credentials.username')).sendKeys('admin');
+  element(by.model('credentials.password')).sendKeys('AdminPassword1!');
+  element(by.model('submitSignIn')).click();
+}
+
 var addJob = function() {
   element(by.model('signInBtn')).click();
   element(by.model('credentials.username')).sendKeys('admin');
@@ -23,7 +30,7 @@ var addReferral = function() {
   element(by.model('submitSignIn')).click();
 }
 
-var addUser = function() {
+var addContact = function() {
   element(by.model('signInBtn')).click();
   element(by.model('credentials.username')).sendKeys('admin');
   element(by.model('credentials.password')).sendKeys('AdminPassword1!');
@@ -35,3 +42,23 @@ describe('SignIn Users', function() {
     browser.get('http://localhost:3000/');
 
     loginAsAdmin();
+}}));
+
+describe('Clicking all the buttons', function(){
+  it('Navigate to About Us', function() {
+    element(by.model('aboutUs')).click;
+    element(by.model('home')).click;
+  });
+  it('Navigate to About Us', function() {
+    element(by.model('services')).click;
+    element(by.model('home')).click;
+  });
+  it('Navigate to About Us', function() {
+    element(by.model('packages')).click;
+    element(by.model('home')).click;
+  });
+  it('Navigate to About Us', function() {
+    element(by.model('careers')).click;
+    element(by.model('home')).click;
+  });
+});

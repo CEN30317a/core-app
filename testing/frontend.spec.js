95349f170b226a5aab938be70279ee3afe9845f9
Daniel Lelis <oheyitsdaniel@gmail.com>
2015-12-08 21:16:01 -0500

    Added test to push a Contact

--- testing/frontend.spec.js
+++ testing/frontend.spec.js
@@ -31,15 +31,27 @@ var addReferral = function() {
 };
 
 var addContact = function() {
-  element(by.model('signInBtn')).click();
-  element(by.model('credentials.username')).sendKeys('admin');
-  element(by.model('credentials.password')).sendKeys('AdminPassword1!');
-  element(by.model('submitSignIn')).click();
+  element(by.model('contact')).click();
+  element(by.model('first_name')).sendKeys('Daniel');
+  element(by.model('last_name')).sendKeys('Lelis');
+  element(by.model('phone')).sendKeys('9048818304');
+  element(by.model('email')).sendKeys('oheyitsdaniel@gmail.com');
+  element(by.model('message')).sendKeys('Man, we so cool');
+  element(by.model('submitContact')).click();
+  element(by.model('home')).click();
 };
 
-describe('Clicking all the buttons', function(){
-  browser.get('http://localhost:3000/');
-    
+browser.get('http://localhost:3000/');
+
+describe('Sending message to admin', function(){
+  it('Navigate to Contact Us page and send message', function(){
+    addContact();
+  });
+});
+
+
+describe('Clicking all the header buttons', function(){
+
   it('Navigate to About Us', function() {
     element(by.model('aboutUs')).click();
     element(by.model('home')).click();
